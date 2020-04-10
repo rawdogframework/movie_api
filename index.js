6d530412bcd052602dc9fe8c6b195ef67f564268
fratzio <thomasmichaelfrazer@gmail.com>
2020-04-09 21:12:34 -0700

    Updated index.js file and documentation with Mongoose queries

--- index.js
+++ index.js
@@ -32,175 +32,169 @@ app.get('/movies', function (req, res) {
 });
 // Get Movies by Title
 app.get('/movies/:title', function (req, res) {
-  let findMovie = topMovies.find((title) => {
-    return title.title === req.params.title;
+  Movies.findOne({ Title: req.params.title }).then(function (title) {
+    if (title) {
+      res.status(200).json(title);
+    } else {
+      const message = 'Title not found in db';
+      res.status(404).send(message);
+    }
   });
-  if (findMovie) {
-    res.json(findMovie);
-  } else {
-    const message = 'Title not found in db';
-    res.status(404).send(message);
-  }
 });
 // Get Genre by Name
 app.get('/movies/genre/:name', function (req, res) {
-  let findGenre = genres.find((genre) => {
-    return genre.genre === req.params.name;
+  Movies.findOne({ 'Genre.Name': req.params.name }).then(function (genre) {
+    if (genre) {
+      res.status(200).json(genre.Genre);
+    } else {
+      const message = 'No Genre matching that name in the db';
+      res.status(404).send(message);
+    }
   });
-  if (findGenre) {
-    res.status(200).json(findGenre);
-  } else {
-    const message = 'No Genre matching that name in the db';
-    res.status(404).send(message);
-  }
 });
 // Get Director by Name
 app.get('/movies/directors/:name', function (req, res) {
-  let findDirector = directors.find((name) => {
-    return name.name === req.params.name;
+  Movies.findOne({ 'Director.Name': req.params.name }).then(function (
+    director
+  ) {
+    if (director) {
+      res.status(200).json(director.Director);
+    } else {
+      const message = 'No Director matching that name in the db';
+      res.status(404).send(message);
+    }
   });
-  if (findDirector) {
-    res.status(200).json(findDirector);
-  } else {
-    const message = 'No Director matching that name in the db';
-    res.status(404).send(message);
-  }
 });
 // Create User Account
 app.post('/users', function (req, res) {
   // db check if user already exists
-  let checkDuplicate = userBase.find((user) => {
-    return user.email === req.body.email;
-  });
-  if (checkDuplicate) {
-    const message =
-      'There is already an account assocaited with this email address';
-    res.status(400).send(message);
-  }
-  // Check if username or pwd value is missing
-  let newUser = {};
-  newUser.email = req.body.email;
-  newUser.pwd = req.body.pwd;
-  newUser.dob = req.body.dob;
-  newUser.username = req.body.username;
-  newUser.favourites = [];
-  if (newUser.email === '' || newUser.pwd === '') {
-    const message = 'Missing email or password in request body';
-    res.status(400).send(message);
-  } else {
-    // create ID for user
-    newUser.id = uuid.v4();
-    // push user to db
-    userBase.push(newUser);
-    res.status(201).json(newUser);
-  }
+  Users.findOne({ Email: req.body.Email })
+    .then(function (user) {
+      if (user) {
+        const message =
+          'There is already an account associated with this email address';
+        res.status(400).send(message);
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then(function (user) {
+            res.status(201).json(user);
+          })
+          .catch(function (error) {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
+
 // Deregister User Account
 app.delete('/users/:id', function (req, res) {
-  if (req.params.id === '') {
-    res.status(400).send('Missing ID in request params');
-  }
-  // search userBase db for match req ID
-  let userToDelete = userBase.find((user) => {
-    return user.id === req.params.id;
-  });
-  if (userToDelete) {
-    // Check pwd and email match req body
-    console.log(userToDelete);
-    console.log(req.body);
-    console.log(req.params.id);
-    if (
-      userToDelete.pwd === req.body.pwd &&
-      userToDelete.email === req.body.email
-    ) {
-      userBase = userBase.filter(function (obj) {
-        return obj.id !== req.params.id;
-      });
-      res.status(201).send('Account ' + req.params.id + ' was deleted.');
-    } else {
-      res
-        .status(404)
-        .send(
-          'Password or Email not matching credentials for the account with the passed ID'
-        );
-    }
-  } else {
-    res
-      .status(404)
-      .send('Account with the username ' + req.body.id + ' was not found.');
-  }
+  Users.findOne({ _id: req.params.id })
+    .then(function (user) {
+      if (user) {
+        // Check password and email match req body
+        if (
+          user.Email === req.body.Email &&
+          user.Password === req.body.Password
+        ) {
+          // Delete user
+          Users.deleteOne(user);
+          const message =
+            'User account with id ' +
+            req.params.id +
+            ' was successfully deleted';
+          return res.status(200).send(message);
+        } else {
+          return res
+            .status(400)
+            .send('Credentials not matching account with that id');
+        }
+      } else {
+        return res.status(400).send('No account matching that id in DB');
+      }
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
+
 // Update User Account
 app.put('/users/update/:id', function (req, res) {
-  if (req.params.id === '') {
-    res.status(400).send('Missing ID in request params');
-  }
-  // search userBase db for match req ID
-  let userToUpdate = userBase.find((user) => {
-    return user.id === req.params.id;
-  });
-  if (userToUpdate) {
-    // update user object
-    userToUpdate.username = req.body.username || userToUpdate.username;
-    userToUpdate.pwd = req.body.pwd || userToUpdate.pwd;
-    userToUpdate.email = req.body.email || userToUpdate.email;
-    userToUpdate.dob = req.body.dob || userToUpdate.dob;
-    // confirmation
-    res.status(201).json(userToUpdate);
-  } else {
-    const message = 'No User matching that ID in the db';
-    res.status(404).send(message);
-  }
+  Users.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true }
+  )
+    .then(function (user) {
+      res.status(200).json(user);
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
+
 // Update Favourites List by Title
 app.post('/users/favourites/:id/', function (req, res) {
-  if (req.params.id === '') {
-    res.status(400).send('Missing ID in request params');
-  }
-  // Search userBase db for match req ID
-  let addToFavourites = userBase.find((user) => {
-    return user.id === req.params.id;
-  });
-  if (addToFavourites) {
-    // Check for duplicates
-
-    addToFavourites.favourites.push(req.body);
-    res.status(201).json(addToFavourites);
-  } else {
-    const message = 'No Account matching that ID in the db.';
-    res.status(404).send(message);
-  }
+  Users.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      /* 
+      Check for duplicates
+      */
+      $push: { FavouriteMovies: req.body.id },
+    },
+    { new: true }
+  )
+    .then(function (updatedUser) {
+      res
+        .status(200)
+        .json('Updated Favourites list: ' + updatedUser.FavouriteMovies);
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
+
 // Remove Movie from Favourites List by Title
 app.delete('/users/favourites/:id/', function (req, res) {
-  if (req.params.id === '') {
-    res.status(400).send('Missing ID in request params');
-  }
-  // Search userBase db for match req ID
-  let removeFromFavourites = userBase.find((user) => {
-    return user.id === req.params.id;
-  });
-  if (removeFromFavourites) {
-    // Check for movie match
-    let checkMovie = removeFromFavourites.favourites.find((movie) => {
-      return movie.title === req.body.title;
+  Users.findOneAndUpdate(
+    { _id: req.params.id },
+    { $pull: { FavouriteMovies: req.body.id } },
+    { new: true }
+  )
+    .then(function (updatedUser) {
+      res
+        .status(200)
+        .json(
+          'Movie with id ' +
+            req.body.id +
+            ' was succesfully deleted. Updated Favourites list: ' +
+            updatedUser.FavouriteMovies
+        );
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     });
-    if (checkMovie) {
-      // Remove from list
-      removeFromFavourites.favourites = removeFromFavourites.favourites.filter(
-        function (obj) {
-          return obj.title !== req.body.title;
-        }
-      );
-      res.status(201).json(removeFromFavourites);
-    } else {
-      const message = 'No match for that movie in your favourites.';
-      res.status(404).send(message);
-    }
-  } else {
-    const message = 'No Account matching that ID in the db.';
-    res.status(404).send(message);
-  }
 });
 
 // listen for requests
