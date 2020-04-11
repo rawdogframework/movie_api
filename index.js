7fd5a541fb29bd738f46501e6ff67dc1034402cb
fratzio <thomasmichaelfrazer@gmail.com>
2020-04-11 14:13:08 -0700

    Adhered to deprecation instructiions for findoneandupdate
    Mongoose's findOneAndUpdate() long pre-dates the MongoDB driver's findOneAndUpdate() function, so it uses the MongoDB driver's findAndModify() function instead. You can opt in to using the MongoDB driver's findOneAndUpdate() function using the useFindAndModify global option.

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ mongoose.connect('mongodb://localhost:27017/victorvilleDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+mongoose.set('useFindAndModify', false);
 
 // Midddleware
 app.use(morgan('common'));
