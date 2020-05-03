f93b2506340b1024a266ef43c6ba3eebf84f5dbf
fratzio <thomasmichaelfrazer@gmail.com>
2020-05-02 17:14:54 -0700

    Added PropTypes check to move-card view - leaving out genre and director checks fo rnow

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
@@ -17,3 +18,23 @@ export class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    // ADD IN ONCE SKELETON WORKS
+    // Genre: PropTypes.shape({
+    //   Name: PropTypes.string.isRequired,
+    //   Description: PropTypes.string.isRequired,
+    // }),
+    // Director: PropTypes.shape({
+    //   Name: PropTypes.string.isRequired,
+    //   Description: PropTypes.string.isRequired,
+    //   Birth: PropTypes.string.isRequired,
+    //   Death: PropTypes.string,
+    // }),
+  }).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
