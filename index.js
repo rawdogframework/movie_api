107f4d04fb6dadb44b2a18183bd575d1bbd804e2
fratzio <thomasmichaelfrazer@gmail.com>
2020-05-06 12:03:43 -0700

    Fixing issue with new logging for req.headers

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), function (
   for (i in req.body) {
     console.log('body == ' + i);
   }
-  console.log(req.header(Authorization));
+  console.log(req.headers);
   Movies.find().then((movies) => res.status(200).json(movies));
 });
 // Get Movies by Title
